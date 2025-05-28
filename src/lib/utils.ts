ac2d9a140b773e6f9eaa4446435676d9c726273b
hendraaagil <hendraaagil@gmail.com>
2025-05-28 21:34:37 +0700

    feat: add utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,24 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes))
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price)
+}
+
+export function pluralize(
+  count: number,
+  singular: string,
+  plural?: string,
+): string {
+  if (count === 1) return singular
+  return plural || `${singular}s`
+}
